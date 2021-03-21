0479940f0940e885f68f5ab912f79166b462121d
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-03-20 20:04:25 -0400

    Added heatmap and modified hyperparameters of random forest regressor based on grid search

--- packages/activelearning/activelearning.py
+++ packages/activelearning/activelearning.py
@@ -1,12 +1,22 @@
 """
 Helper functions to perform active learning with modAL learners.
 """
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
-import csv
 from modAL.models import ActiveLearner, CommitteeRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.metrics import r2_score
 from modAL.disagreement import max_std_sampling
+from modAL.uncertainty import margin_sampling
+
+
+# provided wrapper class
+class RfWrapper(RandomForestRegressor):  # superclass
+    def predict(self, X, return_std=False):
+        if return_std:
+            ys = np.array([e.predict(X) for e in self.estimators_])
+            return np.mean(ys, axis=0).ravel(), np.std(ys, axis=0).ravel()
+        return super().predict(X).ravel()
 
 
 def get_next_sample(learner, X, y):
@@ -40,7 +50,6 @@ def run_active_learner_regression(learner, X_pool, y_pool, n_queries):
 
 
 def run_and_score_active_learner_regression(learner, X_pool, y_pool, X_test, y_test, n_queries):
-
     history = []
 
     # score before starting
@@ -88,3 +97,22 @@ def score_regression_model(learner, X_test, y_test):
     y_pred = learner.predict(X_test, return_std=False)
     r2 = r2_score(y_test, y_pred)  # y_true, y_pred
     return r2
+
+
+def build_random_forest_regressor(n_estimators, max_depth, n_initial, X_pool, y_pool, seed):
+    initial_idx = np.random.choice(len(X_pool), size=n_initial, replace=False)
+
+    # https://modal-python.readthedocs.io/en/latest/content/examples/active_regression.html
+    def GP_regression_std(regressor, X):
+        _, std = regressor.predict(X, return_std=True)
+        query_idx = np.argmax(std)
+        return query_idx, X[query_idx]
+
+    regressor = ActiveLearner(
+        estimator=RfWrapper(n_estimators=n_estimators, max_depth=max_depth, random_state=seed),
+        query_strategy=GP_regression_std,
+        X_training=X_pool[initial_idx],
+        y_training=y_pool[initial_idx]
+    )
+
+    return regressor
--- packages/gridsearch/gridsearch.py
+++ packages/gridsearch/gridsearch.py
@@ -19,10 +19,17 @@ def _append_results_to_file(filename, fields=None, rows=None):
 
 
 def _create_row_gs1(kernel, n_learner, n_initial, n_queries, row_prefix, details_func, r2):
-
     kernel_hyperparms = details_func(kernel)
     meta = [n_learner, n_initial, n_queries, r2]
     row = row_prefix + kernel_hyperparms + meta
+
+    return [row]
+
+
+def _create_row_gs2(n_estimator,max_depth, n_initial, n_queries, row_prefix, r2):
+    hyperparms = [n_estimator, max_depth]
+    meta = [1, n_initial, n_queries, r2]
+    row = row_prefix + hyperparms + meta
 
     return [row]
 
@@ -57,7 +64,6 @@ def grid_search_1(kernels, n_learners, n_initials, X_pool, y_pool, X_test, y_tes
     for kernel in kernels:
         for n_learner in n_learners:
             for n_initial in n_initials:
-
                 # make a copy of the data for use in this experiment
                 X_pool_gs = copy.deepcopy(X_pool)
                 y_pool_gs = copy.deepcopy(y_pool)
@@ -79,3 +85,57 @@ def grid_search_1(kernels, n_learners, n_initials, X_pool, y_pool, X_test, y_tes
 
                 # output to console for tracking progress
                 print('{}|{}|{}|{}|{}'.format(kernel, n_learner, n_initial, n_queries, r2))
+
+
+def grid_search_2(n_estimators, max_depths, n_initials, X_pool, y_pool, X_test, y_test, n_queries,
+                  seed, filename, fields, row_prefix):
+    """
+    Grid search over (kernels, n_learners, n_initials).
+    Appends results to file after each run combination. Experiment can be halted at any time without losing results.
+    Uses Random Forest Regressor for active learner.
+
+    :param kernels:
+    :param n_learners:
+    :param n_initials:
+    :param X_pool:
+    :param y_pool:
+    :param X_test:
+    :param y_test:
+    :param n_queries:
+    :param seed:
+    :param filename:
+    :param fields:
+    :param row_prefix:
+    :param details_func:
+    :return:
+    """
+
+    # append fields as first row in file
+    _append_results_to_file(filename, fields)
+
+    # perform grid search
+    for n_estimator in n_estimators:
+        for max_depth in max_depths:
+            for n_initial in n_initials:
+                # make a copy of the data for use in this experiment
+                X_pool_gs = copy.deepcopy(X_pool)
+                y_pool_gs = copy.deepcopy(y_pool)
+
+                # build learner
+                regressor = al.build_random_forest_regressor(n_estimator, max_depth, n_initial, X_pool_gs, y_pool_gs,
+                                                             seed)
+
+                # perform active learning
+                al.run_active_learner_regression(regressor, X_pool_gs, y_pool_gs, n_queries)
+
+                # score model
+                r2 = al.score_regression_model(regressor, X_test, y_test)
+
+                # create row for file
+                row = _create_row_gs2(n_estimator,max_depth, n_initial, n_queries, row_prefix, r2)
+
+                # append to file
+                _append_results_to_file(filename, rows=row)
+
+                # output to console for tracking progress
+                print('RandomForestRegressor(n_estimators={},max_depth={})|{}|{}|{}'.format(n_estimator,max_depth, n_initial, n_queries, r2))
