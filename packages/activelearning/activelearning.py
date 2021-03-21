c5ef8623d8501816045584c77bf54f1d88e05d4d
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-03-21 12:37:02 -0400

    Documentation

--- packages/activelearning/activelearning.py
+++ packages/activelearning/activelearning.py
@@ -9,8 +9,13 @@ from sklearn.metrics import r2_score
 from modAL.disagreement import max_std_sampling
 
 
-# provided wrapper class
 class RfWrapper(RandomForestRegressor):  # superclass
+    """
+    Wrapper class for RandomForestRegressor which modifies predict() method to include second argument return_std.
+    This argument is expected by
+    modAL library for active learning regression. Provided by course instructors.
+    """
+
     def predict(self, X, return_std=False):
         if return_std:
             ys = np.array([e.predict(X) for e in self.estimators_])
@@ -19,6 +24,14 @@ class RfWrapper(RandomForestRegressor):  # superclass
 
 
 def get_next_sample(learner, X, y):
+    """
+    Queries the pool X of data and selects a new sample using the query_strategy of the ActiveLearner.
+
+    :param learner:the ActiveLearner within which a query_strategy is defined.
+    :param X:the pool of data from which to select a sample. This is a numpy array of feature instances.
+    :param y:the pool of labels corresponding to X instances. This is a numpy array of labels.
+    :return: (X,y,idx) tuple of the selected sample, where idx is the index of the selected sample.
+    """
     # call the query strategy defined in the learner to obtain a new sample
     query_idx, query_sample = learner.query(X)
 
@@ -35,6 +48,17 @@ def get_next_sample(learner, X, y):
 
 
 def run_active_learner_regression(learner, X_pool, y_pool, n_queries):
+    """
+    Performs active learning using given ActiveLearner. Runs for
+    the given number of queries. Each iteration draws from the pool of
+    data using the learner's query_strategy, updates the model, then removes queried instance from the data pool.
+
+    :param learner: the ActiveLearner
+    :param X_pool:the pool of feature data from which to sample
+    :param y_pool:the labels corresponding to the X_pool
+    :param n_queries:the number of queries (iterations) to execute during active learning
+    :return: None
+    """
     # perform active learning
     for q in range(n_queries):
         # get sample
@@ -49,6 +73,20 @@ def run_active_learner_regression(learner, X_pool, y_pool, n_queries):
 
 
 def run_and_score_active_learner_regression(learner, X_pool, y_pool, X_test, y_test, n_queries):
+    """
+    Performs active learning using given ActiveLearner. Runs for
+    the given number of queries. Each iteration draws from the pool of
+    data using the learner's query_strategy, updates the model, removes queried instance from the data pool,
+    then scores the model against given test data.
+
+    :param learner: the ActiveLearner
+    :param X_pool:the pool of feature data from which to sample
+    :param y_pool:the labels corresponding to the X_pool
+    :param X_test:the collection of data with which to score the model
+    :param y_test:the labels corresponding to the X_test
+    :param n_queries:the number of queries (iterations) to execute during active learning
+    :return: List of scores for each query.
+    """
     history = []
 
     # score before starting
@@ -75,6 +113,19 @@ def run_and_score_active_learner_regression(learner, X_pool, y_pool, X_test, y_t
 
 
 def build_committee(kernel, n_learner, n_initial, X_pool, y_pool, seed):
+    """
+    Constructs a CommitteeRegressor of ActiveLearner members based on provided parameters.
+    Uses GaussianProcessRegressors as committee members.
+    Defines initial training set of random instances for each learner in the committee.
+
+    :param kernel: Kernel to be used in Gaussian Process regressors.
+    :param n_learner: Number of members in the committee.
+    :param n_initial: Number of initial training instances for each committee member.
+    :param X_pool:the pool of feature data from which to sample
+    :param y_pool:the labels corresponding to the X_pool
+    :param seed: Random seed for reproducibility.
+    :return: CommitteeRegressor
+    """
     # get initial training set for each learner
     initial_idx = []
     for i in range(n_learner):
@@ -88,17 +139,36 @@ def build_committee(kernel, n_learner, n_initial, X_pool, y_pool, seed):
 
     # create Committee
     committee = CommitteeRegressor(learner_list=learner_list, query_strategy=max_std_sampling)
-
     return committee
 
 
 def score_regression_model(learner, X_test, y_test):
+    """
+    Calculates R2 score for given learner.
+
+    :param learner: ActiveLearner
+    :param X_test: Test set
+    :param y_test: Regression values for test set
+    :return: Float, r2 score.
+    """
     y_pred = learner.predict(X_test, return_std=False)
     r2 = r2_score(y_test, y_pred)  # y_true, y_pred
     return r2
 
 
 def build_random_forest_regressor(n_estimators, max_depth, n_initial, X_pool, y_pool, seed):
+    """
+    Constructs RandomForestRegressor ActiveLearner with custom query_strategy.
+    Defines initial training set of random instances for learner.
+
+    :param n_estimators: Number of estimators in Random Forest.
+    :param max_depth: Max depth of trees in Random Forest.
+    :param n_initial: Number of initial training instances for each committee member.
+    :param X_pool:the pool of feature data from which to sample
+    :param y_pool:the labels corresponding to the X_pool
+    :param seed: Random seed for reproducibility.
+    :return: ActiveLearner
+    """
     initial_idx = np.random.choice(len(X_pool), size=n_initial, replace=False)
 
     # https://modal-python.readthedocs.io/en/latest/content/examples/active_regression.html
